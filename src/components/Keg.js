27c2c03975e6d264f9efea0d62105d32df94b507
Woo Jin Kim <300hHZ@gmail.com>
2021-04-23 09:59:30 -0700

    set up base Keg, KegList, & KegControl files

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -2,9 +2,20 @@ import React from "react";
 
 function Keg() {
   return (
-    <React.Fragment>
+    <React.Fragment>      
+      <h3>{props.brand} - {props.name}</h3>
+      <p><em>{props.alcoholContent}</em></p>
+      <p><em>{props.price}</em></p>
+      <hr />
     </React.Fragment>
   );
 }
+
+Ticket.propTypes = {
+  name: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  alcoholContent: PropTypes.string.isRequired,
+  price: PropTypes.string
+};
 
 export default Keg;
