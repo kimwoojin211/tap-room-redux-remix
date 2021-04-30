9630433f95a1318c78f0b0b1878621701e6c8284
Woo Jin Kim <300hHZ@gmail.com>
2021-04-30 09:44:24 -0700

    pass addKegData test

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,19 @@
 export default (state = {}, action) => {
-  return state;
+    const { name, brand, alcoholContent, price, id, pints } = action;
+    switch (action.type){
+
+    case 'ADD_KEG':
+      return Object.assign({},state,{
+        [id]: {
+          name: name,
+          brand: brand,
+          alcoholContent: alcoholContent,
+          price: price,
+          id: id,
+          pints: pints
+        }
+      });
+    default:
+      return state;
+    }
 };
