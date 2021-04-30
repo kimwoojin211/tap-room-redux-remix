b6832279dfc40e77633100a40881396c7addea1c
Woo Jin Kim <300hHZ@gmail.com>
2021-04-30 10:18:54 -0700

    add and fail deleteKeg test

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -10,6 +10,24 @@ describe('kegListReducer', () => {
     id: 1,
     pints: 124
   };
+  const currentState = {
+    1:{
+    name: "Keggie",
+    brand: "Keggles",
+    alcoholContent: "96",
+    price: "3000",
+    id: 1,
+    pints: 124
+    },
+    2: {
+      name: "Kegtropolis",
+      brand: "Keggles",
+      alcoholContent: "200",
+      price: "46290",
+      id: 2,
+      pints: 124
+    }
+  };
 
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, {type: null })).toEqual({});
@@ -35,6 +53,23 @@ describe('kegListReducer', () => {
         price: price,
         id: id,
         pints: pints
+      }
+    });
+  });
+
+  test('Should successfully delete a keg', () => {
+    action = {
+      type: 'DELETE_KEG',
+      id: 1
+    };
+    expect(kegListReducer(currentState, action)).toEqual({
+      2: {
+        name: "Kegtropolis",
+        brand: "Keggles",
+        alcoholContent: "200",
+        price: "46290",
+        id: 2,
+        pints: 124
       }
     });
   });
